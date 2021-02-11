1b067b90d43c05995b5182f7c3d6e2fea8f728f9
ratneshmurugesan <Ratnesh.one@gmail.com>
2021-02-11 17:42:47 +0530

    feat: CSS Grid - pro level

--- src/components/5prototypes/CSSLayouts/index.jsx
+++ src/components/5prototypes/CSSLayouts/index.jsx
@@ -104,15 +104,144 @@ const level3 = (
 );
 
 const level4 = (
-    <div className="wrapper-level-4">
-      <div className="box box1">BOX 1</div>
-      <div className="box box2">BOX 2</div>
-      <div className="box box3">BOX 3</div>
-      <div className="box box4">BOX 4</div>
-    </div>
-  );
+  <div className="wrapper-level-4">
+    <div className="box box1">BOX 1</div>
+    <div className="box box2">BOX 2</div>
+    <div className="box box3">BOX 3</div>
+    <div className="box box4">BOX 4</div>
+  </div>
+);
+
+const level5 = (
+  <div className="wrapper-level-5">
+    <nav className="main-nav">
+      <ul>
+        <li>
+          <a href="/">NavMenu1</a>
+        </li>
+        <li>
+          <a href="/">NavMenu2</a>
+        </li>
+        <li>
+          <a href="/">NavMenu3</a>
+        </li>
+        <li>
+          <a href="/">NavMenu4</a>
+        </li>
+      </ul>
+    </nav>
+
+    <section className="top-container">
+      <header className="showcase">
+        <h1>HEADER 1</h1>
+        <p>
+          Magna veniam quis ipsum minim incididunt pariatur. Veniam id
+          incididunt et culpa excepteur irure minim esse mollit enim Lorem.
+          Eiusmod id officia occaecat anim tempor in quis sint labore officia
+          esse nisi quis. Exercitation sunt consectetur deserunt velit aliqua
+          nisi dolore enim fugiat amet adipisicing consectetur.
+        </p>
+        <a href="/" className="btn">
+          Read More
+        </a>
+      </header>
+      <div className="top-box top-box-a">
+        <h4>Header 2</h4>
+        <p className="price">$199/mo</p>
+        <a href="/" className="btn">
+          Buy Now
+        </a>
+      </div>
+      <div className="top-box top-box-b">
+        <h4>Header 3</h4>
+        <p className="price">$299/mo</p>
+        <a href="/" className="btn">
+          Buy Now
+        </a>
+      </div>
+    </section>
+
+    <section className="boxes">
+      <div className="box">
+        <h3>one</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="box">
+        <h3>two</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="box">
+        <h3>three</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className="box">
+        <h3>four</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta quae
+          sunt nostrum, recusandae cum quod doloribus minus cumque asperiores
+          mollitia nobis incidunt nemo provident labore saepe consequatur ad
+          iste nisi.
+        </p>
+      </div>
+      <div className="box">
+        <h3>five</h3>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta quae
+          sunt nostrum, recusandae cum quod doloribus minus cumque asperiores
+          mollitia nobis incidunt nemo provident labore saepe consequatur ad
+          iste nisi.
+        </p>
+      </div>
+    </section>
+
+    <section className="info">
+      <img
+        alt="clip"
+        id="clipped"
+        alt="clip"
+        className="clip"
+        src="https://source.unsplash.com/500x500/?nature"
+      />
+      <div>
+        <h2>BBB</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
+          ut neque voluptate autem recusandae, atque unde blanditiis omnis magni
+          enim quibusdam facilis consequuntur rerum ipsam? Nam aliquid itaque
+          consequatur perspiciatis!
+        </p>
+        <a href="/" className="btn">
+          Learn More
+        </a>
+      </div>
+    </section>
+
+    <section className="portfolio">
+      <img alt="clip" src="https://source.unsplash.com/200x200/?nature" />
+      <img alt="clip" src="https://source.unsplash.com/201x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/202x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/203x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/204x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/205x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/206x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/207x200/?nature" />{" "}
+      <img alt="clip" src="https://source.unsplash.com/208x200/?nature" />
+    </section>
+
+    <footer>
+      <p>Grid copyrights</p>
+    </footer>
+  </div>
+);
+
 const CSSLayouts = () => {
-  return level4;
+  return level5;
 };
 
 export default CSSLayouts;
