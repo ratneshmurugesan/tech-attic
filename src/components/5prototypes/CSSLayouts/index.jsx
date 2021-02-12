59475a8c16e59b3ece0bf09fef6dce4620c8215a
ratneshmurugesan <Ratnesh.one@gmail.com>
2021-02-12 09:15:59 +0530

    fix: CSS Layouts: CSS Grid

--- src/components/5prototypes/CSSLayouts/index.jsx
+++ src/components/5prototypes/CSSLayouts/index.jsx
@@ -41,76 +41,76 @@ import "./index.scss";
 //     )
 // };
 
-const level1 = (
-  <div className="wrapper-level-1">
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>
-      Excepteur elit aliqua cupidatat proident ea aliqua duis. In ipsum
-      excepteur ea non velit velit. Amet amet esse fugiat elit deserunt enim.
-      Commodo irure culpa duis nostrud irure elit. Lorem in voluptate fugiat
-      culpa aute aute dolor proident eiusmod ea. Id sit nisi nulla nulla nulla
-      sint culpa. Minim sit eiusmod sunt exercitation elit sit velit id ex eu id
-      Lorem ut culpa. Ea mollit et reprehenderit sit sunt officia consequat
-      cupidatat amet labore deserunt. Amet mollit amet eiusmod voluptate
-      consequat excepteur ipsum aute in eiusmod est. Elit anim esse quis tempor
-      sunt non. Commodo exercitation esse officia laboris sunt magna dolore
-      magna ipsum. Adipisicing adipisicing adipisicing voluptate excepteur. Aute
-      id consectetur eu voluptate non irure sunt. Amet id ea sunt anim.
-    </div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-  </div>
-);
+// const level1 = (
+//   <div className="wrapper-level-1">
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>
+//       Excepteur elit aliqua cupidatat proident ea aliqua duis. In ipsum
+//       excepteur ea non velit velit. Amet amet esse fugiat elit deserunt enim.
+//       Commodo irure culpa duis nostrud irure elit. Lorem in voluptate fugiat
+//       culpa aute aute dolor proident eiusmod ea. Id sit nisi nulla nulla nulla
+//       sint culpa. Minim sit eiusmod sunt exercitation elit sit velit id ex eu id
+//       Lorem ut culpa. Ea mollit et reprehenderit sit sunt officia consequat
+//       cupidatat amet labore deserunt. Amet mollit amet eiusmod voluptate
+//       consequat excepteur ipsum aute in eiusmod est. Elit anim esse quis tempor
+//       sunt non. Commodo exercitation esse officia laboris sunt magna dolore
+//       magna ipsum. Adipisicing adipisicing adipisicing voluptate excepteur. Aute
+//       id consectetur eu voluptate non irure sunt. Amet id ea sunt anim.
+//     </div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//   </div>
+// );
 
-const level2 = (
-  <div className="wrapper-level-2">
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div className="nested">
-      <div>Lorem</div>
-      <div>Lorem</div>
-      <div>Lorem</div>
-      <div>Lorem</div>
-      <div>Lorem</div>
-      <div>Lorem</div>
-    </div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>
-      Excepteur elit aliqua cupidatat proident ea aliqua duis. In ipsum
-      excepteur ea non velit velit. Amet amet esse fugiat elit deserunt enim.
-      Commodo irure culpa duis nostrud irure elit. Lorem in voluptate fugiat
-      culpa aute aute dolor proident eiusmod ea. Id sit nisi nulla nulla nulla
-      sint culpa. Minim sit eiusmod sunt exercitation elit sit velit id ex eu id
-      Lorem ut culpa. Ea mollit et reprehenderit sit sunt officia consequat
-      cupidatat amet labore deserunt. Amet mollit amet eiusmod voluptate
-      consequat excepteur ipsum aute in eiusmod est. Elit anim esse quis tempor
-      sunt non. Commodo exercitation esse officia laboris sunt magna dolore
-      magna ipsum. Adipisicing adipisicing adipisicing voluptate excepteur. Aute
-      id consectetur eu voluptate non irure sunt. Amet id ea sunt anim.
-    </div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-    <div>Nisi excepteur reprehenderit non nulla consequat.</div>
-  </div>
-);
+// const level2 = (
+//   <div className="wrapper-level-2">
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div className="nested">
+//       <div>Lorem</div>
+//       <div>Lorem</div>
+//       <div>Lorem</div>
+//       <div>Lorem</div>
+//       <div>Lorem</div>
+//       <div>Lorem</div>
+//     </div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>
+//       Excepteur elit aliqua cupidatat proident ea aliqua duis. In ipsum
+//       excepteur ea non velit velit. Amet amet esse fugiat elit deserunt enim.
+//       Commodo irure culpa duis nostrud irure elit. Lorem in voluptate fugiat
+//       culpa aute aute dolor proident eiusmod ea. Id sit nisi nulla nulla nulla
+//       sint culpa. Minim sit eiusmod sunt exercitation elit sit velit id ex eu id
+//       Lorem ut culpa. Ea mollit et reprehenderit sit sunt officia consequat
+//       cupidatat amet labore deserunt. Amet mollit amet eiusmod voluptate
+//       consequat excepteur ipsum aute in eiusmod est. Elit anim esse quis tempor
+//       sunt non. Commodo exercitation esse officia laboris sunt magna dolore
+//       magna ipsum. Adipisicing adipisicing adipisicing voluptate excepteur. Aute
+//       id consectetur eu voluptate non irure sunt. Amet id ea sunt anim.
+//     </div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//     <div>Nisi excepteur reprehenderit non nulla consequat.</div>
+//   </div>
+// );
 
-const level3 = (
-  <div className="wrapper-level-3">
-    <div className="box box1">BOX 1</div>
-    <div className="box box2">BOX 2</div>
-    <div className="box box3">BOX 3</div>
-    <div className="box box4">BOX 4</div>
-  </div>
-);
+// const level3 = (
+//   <div className="wrapper-level-3">
+//     <div className="box box1">BOX 1</div>
+//     <div className="box box2">BOX 2</div>
+//     <div className="box box3">BOX 3</div>
+//     <div className="box box4">BOX 4</div>
+//   </div>
+// );
 
-const level4 = (
-  <div className="wrapper-level-4">
-    <div className="box box1">BOX 1</div>
-    <div className="box box2">BOX 2</div>
-    <div className="box box3">BOX 3</div>
-    <div className="box box4">BOX 4</div>
-  </div>
-);
+// const level4 = (
+//   <div className="wrapper-level-4">
+//     <div className="box box1">BOX 1</div>
+//     <div className="box box2">BOX 2</div>
+//     <div className="box box3">BOX 3</div>
+//     <div className="box box4">BOX 4</div>
+//   </div>
+// );
 
 const level5 = (
   <div className="wrapper-level-5">
@@ -204,7 +204,6 @@ const level5 = (
       <img
         alt="clip"
         id="clipped"
-        alt="clip"
         className="clip"
         src="https://source.unsplash.com/500x500/?nature"
       />
